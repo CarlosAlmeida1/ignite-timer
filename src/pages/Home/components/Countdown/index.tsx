934641dfa3a03cd3cb7645754c1fc2cbc924738d
Carlos Henrique <carloshenriquealmeida52@gmail.com>
2023-10-07 13:32:15 -0300

    create: action types

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -56,7 +56,7 @@ export function Countdown() {
 
   useEffect(() => {
     if (activeCycle) {
-      document.title = `${minutes}:${seconds}`;
+      document.title = `${minutes}:${seconds} | Ignite Timer`;
     }
   }, [minutes, seconds, activeCycle]);
 
